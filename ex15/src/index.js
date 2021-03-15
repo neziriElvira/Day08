abf61cdb5460d60a6110bc68979a6342cf783063
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-15 21:33:04 +0100

    Corrected the mistake

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -3,7 +3,7 @@ function convertToInt(str) {
     return parseInt(str, 16);
 }
 convertToInt("BA");
-convertToInt("F1"));
+convertToInt("F1");
 convertToInt("JeffBezos");
 
 // Only change code above this line
